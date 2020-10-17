c6b4c28cb8eee85e87ff1ae3881fc762e754cf45
Mayur <mayur.takawale@flexiloans.com>
2020-10-17 08:37:28 +0530

    added sckeleton

---
+++ middleware/response.middleware.js
@@ -0,0 +1,28 @@
+module.exports = async (request, response, next) => {
+	try {
+		const oldJson = response.json;
+
+		response.json = (data, message) => {
+			let newResponseData = {};
+			if (!data || data.code === 'ERROR') {
+				newResponseData = {
+					success: false,
+					error: data
+				};
+			} else {
+				newResponseData = {
+					success: true,
+					message: message || '',
+					data
+				};
+			}
+
+			response.json = oldJson;
+
+			return oldJson.call(response, newResponseData);
+		};
+		next();
+	} catch (error) {
+		next(error);
+	}
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+Readme for Appointment Booking
